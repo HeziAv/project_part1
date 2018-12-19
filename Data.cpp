87e77cba8ccb53354b22566ea075271ea601229b
Hezi Avraham <chezi91@gmail.com>
2018-12-19 17:08:04 +0200

    hy

---
+++ Data.cpp
@@ -0,0 +1,11 @@
+//
+// Created by hezi on 12/19/18.
+//
+
+#include "Data.h"
+
+Data ::Data(map<string, double> SymTbl, map<string, string> bindMap) {
+    this->SymTbl = SymTbl;
+    this->bindMap = bindMap;
+}
+
---
+++ Data.h
@@ -0,0 +1,23 @@
+//
+// Created by hezi on 12/19/18.
+//
+
+#ifndef PROJECT_PART1_DATA_H
+#define PROJECT_PART1_DATA_H
+
+
+#include "ExpressionCommand.h"
+
+class Data {
+    map<string,double> SymTbl;
+    map<string,string> bindMap;
+
+public:
+    Data(map<string,double> SymTbl,map<string,string> bindMap){};
+
+
+
+};
+
+
+#endif //PROJECT_PART1_DATA_H
