1eeb22106723a20f94f1097d21487f60466f0bb4
Hezi Avraham <chezi91@gmail.com>
2018-12-20 14:22:36 +0200

    hy

--- Interpeter.cpp
+++ Interpeter.cpp
@@ -25,26 +25,38 @@ list<string> Interpeter::lexer(string str) {
             if(object == "bind"){
                 break;
             }
-            if((str[index] >= 65 && str[index]<= 90) || (str[index]>=  97 && str[index<=122])){
-                var = 1;
-                if(number == 1){
-                    cout << "error" <<endl;
-                    throw (0);
+            // check if the char is letter
+            if((str[index] >= 65 && str[index]<= 90) || (str[index]>=  97 && str[index]<=122)){
+
+                if(object == ""){
+                    var = 1; // start reading a variable
+                }
+                // error in case that is not fix
+                if(var == 0 && number == 1){
+                    cout<<"error"<<endl;
+                    throw 0;
                 }
                 object.push_back(str[index]);
                 index++;
                 continue;
+            // check if the char is number
             } else if(str[index] >= 48 && str[index]<= 57){
-                number = 1;
+                if(object == ""){
+                    number = 1;
+                }
                 object.push_back(str[index]);
                 index++;
                 continue;
             } else{
-                if(var == 1){
+                if(number == 0 && var == 1){
                     if(str[index] == '_'){
                         object.push_back(str[index]);
                         index++;
                         continue;
+                    } else if (str[index] == ','){}
+                    else{
+                        cout<<"error"<<endl;
+                        throw 0;
                     }
                 }
                 if(number == 1){
@@ -52,10 +64,22 @@ list<string> Interpeter::lexer(string str) {
                         object.push_back(str[index]);
                         index++;
                         continue;
+                    }else if (isOperator(str[index])){}
+                    else{
+                        cout<<"error"<<endl;
+                        throw 0;
                     }
                 }
                 if(object != ""){
                   listOfStrings.push_back(object);
+                }
+                if(isOperator(str[index])){
+                    --index;
+                    if(str[index]!= '-'){
+                        cout<<"error"<<endl;
+                        throw 0;
+                    }
+                    index++;
                 }
                 object=str[index];
                 listOfStrings.push_back(object);
@@ -87,6 +111,27 @@ list<string> Interpeter::lexer(string str) {
             object.push_back(str[i]);
         }
         listOfStrings = miniLexer(listOfStrings);
+        //remove the char : , from the list
+        list<string>::iterator it;
+        for (it = listOfStrings.begin();it != listOfStrings.end();++it) {
+            string temp = *it;
+            string newString = "";
+            if(*it == "bind"){
+                break;
+            }
+            for (long i = 0; i < temp.length() ; ++i) {
+                if(temp.length() == 1){
+                    if(!(temp[i] >= 48 && temp[i]<= 57) || ((temp[i] >= 65 && temp[i]<= 90) || (temp[i]>=  97 && temp[i]<=122))){
+                        cout<<"error"<<endl;
+                        throw 0;
+                    }
+                }
+                if(temp[i] != ','){
+                    newString.push_back(temp[i]);
+                }
+            }
+            *it = newString;
+        }
         return listOfStrings;
 }
 
@@ -107,7 +152,7 @@ list<string> Interpeter::miniLexer(list<string> ls) {
                 flag = 1;
                 --it;
                 string temp = *it;
-                if(temp == "=" ){
+                if(temp == "="){
                   cout<<"error"<<endl;
                   throw (0);
                 }
@@ -121,11 +166,10 @@ list<string> Interpeter::miniLexer(list<string> ls) {
                 newList.push_back(newString);
 
             }else {
-
                 list<string>::iterator iit = newList.end();
                 --iit;
                 string check = *iit;
-                if(check == "=" ){
+                if(check == "="){
                     cout<<"error"<<endl;
                     throw (0);
                 }
@@ -166,4 +210,12 @@ void Interpeter::parser(list<string> ls) {
 
 
 
+}
+
+
+bool Interpeter::isOperator(char s){
+    if (s == '+' || s == '-' || s == '/' || s == '*' || s == ','){
+        return true;
+    }
+    return false;
 }
